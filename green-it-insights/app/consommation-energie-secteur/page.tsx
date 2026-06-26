6345e2e8b05870d4e90d2600e113fbf01fc787bb
Nolan910 <115090897+Nolan910@users.noreply.github.com>
2026-06-26 18:35:24 +0200

    ScrollReveal

--- green-it-insights/app/consommation-energie-secteur/page.tsx
+++ green-it-insights/app/consommation-energie-secteur/page.tsx
@@ -33,20 +33,6 @@ export default function ConsommationEnergiePage() {
 
         </Box>
 
-        <Box mt="8">
-          <SecteurExplications />
-        </Box>
-
-        <Box mt="8">
-          <EvolutionNumeriqueChart />
-        </Box>
-
-        <Box mt="8">
-            <EvolutionNumeriqueDetails />
-        </Box>
-
-        
-
       </main>
     </Container>
   );
